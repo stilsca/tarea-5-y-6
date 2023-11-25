ff907b4dca7aaf55076ea8deb3bf60d1da63aae9
stilsca <40442286+stilsca@users.noreply.github.com>
2023-11-24 23:39:41 -0300

    Add files via upload

---
+++ tallerphp08/php/navbar.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/tallerphp08/">CRUD</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/tallerphp08/">Inicio</a>
+            </li>
+            <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Estudiantes
+            </a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="/tallerphp08/php/estudiante/create.php">Agregar</a></li>
+                <li><hr class="dropdown-divider"></li>                
+                <li><a class="dropdown-item" href="/tallerphp08/php/estudiante/">Listar</a></li>
+                <li><a class="dropdown-item" href="#">Imprimimr</a></li>
+            </ul>
+            </li>
+        </ul>
+        </div>
+    </div>
+    </nav>
